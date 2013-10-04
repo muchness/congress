fbcfbe4bd4fd9eebe890592741f8b91c57eaaae7
Thomas Anthony <twa.photo@gmail.com>
2013-10-04 10:49:43 -0700

    updated legislators

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -2,7 +2,16 @@ require_relative '../../db/config'
 
 class Legislator < ActiveRecord::Base
 
-  scope :active, ->{ where('in_office = ?', 1) }
+  scope :active, ->{ where(in_office: '1') }
+  scope :inactive, ->{ where(in_office: '0') }
+  scope :senator, ->{ where(title: 'Sen') }
+  scope :representative, ->{ where(title: 'Rep') }
+  scope :male, ->{ where(gender: 'M') }
+  scope :female, ->{ where(gender: 'F') }
+  scope :republican, ->{ where(party: 'R')}
+  scope :democrat, ->{ where(party: 'D')}
+  scope :independent, ->{ where(party: 'I')}
+
 
   # def self.active
   #   Legislator.where('in_office = ?', 1).each do |leg|
@@ -23,21 +32,17 @@ class Legislator < ActiveRecord::Base
       puts "#{leg.name}"
     end  
   end
-
-  def self.retired 
-    puts "What state are you from?"
-    state = gets.chomp
-    puts "Gone but not forgotten:"
-
-  end  
-
 end
 
-# Class Senators < Legislators
-# end
+Legislator.independent.each do |leg|
+  puts "#{leg.name} #{leg.party}"
+end 
 
-# Class Representatives < Legislators
-# end
-# person = Legislator.find(6)
-# p person.name
-p Legislator.active.name
+
+
+
+
+
+
+
+
